dbe783e65d892d7404400011838ea16a6b2d501f
ChaitanyaM2004 <chaitanyajly0904@gmail.com>
2025-08-23 15:41:06 +0530

    initial commit and customer entity creation

---
+++ src/main/java/com/urbancartel/UrbanCartelApplication.java
@@ -0,0 +1,13 @@
+package com.urbancartel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanCartelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanCartelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbancartel/UrbanCartelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbancartel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanCartelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
